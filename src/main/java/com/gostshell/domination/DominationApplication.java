91a819f7a31d1062f0cd0fee6479583e3f1478ae
john-weetaker <want4planch@gmail.com>
2019-01-29 08:15:49 +0100

    first commit

---
+++ src/main/java/com/gostshell/domination/DominationApplication.java
@@ -0,0 +1,14 @@
+package com.gostshell.domination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominationApplication.class, args);
+	}
+
+}
+
